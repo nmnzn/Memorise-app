216d091d1470ea0ca978be3c3a156c82591e2ddc
neil <neil.manzano@outlook.fr>
2026-03-09 16:22:43 +0100

    init: fin

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_09_142018) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_09_150542) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.bigint "card_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.boolean "value"
+    t.index ["card_id"], name: "index_answers_on_card_id"
+    t.index ["user_id"], name: "index_answers_on_user_id"
+  end
+
+  create_table "cards", force: :cascade do |t|
+    t.string "ask"
+    t.datetime "created_at", null: false
+    t.bigint "memo_id", null: false
+    t.string "question"
+    t.datetime "updated_at", null: false
+    t.index ["memo_id"], name: "index_cards_on_memo_id"
+  end
+
+  create_table "memos", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_memos_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -25,4 +52,9 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_09_142018) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "answers", "cards"
+  add_foreign_key "answers", "users"
+  add_foreign_key "cards", "memos"
+  add_foreign_key "memos", "users"
 end

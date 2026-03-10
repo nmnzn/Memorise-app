1e1fd422a60451450bdbbefa0032b197f2678d7e
neil <neil.manzano@outlook.fr>
2026-03-10 11:27:53 +0100

    crud

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -15,6 +15,12 @@ class MemosController < ApplicationController
     @memo.user = current_user
   end
 
+  def destroy
+    @memo = Memo.find(params[:id])
+    @memo.destroy
+    redirect_to memos_path
+  end
+
   private
 
   def memo_params

f2c5145fe4188ec76d92904bcea6df1dbf78c96e
MM <maximilien.moreau@outlook.com>
2026-03-10 10:56:51 +0100

    Memo creation

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -3,5 +3,28 @@ class MemosController < ApplicationController
     @memos = current_user.memos
   end
 
-  
+  def show
+    @memo = Memo.find(params[:id])
+  end
+
+  def new
+    @memo = Memo.new
+  end
+
+  def create
+    @memo = Memo.new(memo_params)
+    @memo.user = current_user
+
+    if @memo.save
+      redirect_to memos_path, notice: "Le mémo a bien été créé."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def memo_params
+    params.require(:memo).permit(:name)
+  end
 end

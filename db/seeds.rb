b08b087f30a3b20e81ba82f8a55bc6eacad4a7d6
Mathis <kmzmathis@gmail.com>
2026-03-10 10:52:33 +0100

    cards table updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require "faker"
 
+# creation d'un user TEST pour le développement
 User.where.not(email: "test@test.com").destroy_all
 
 user = User.find_or_create_by!(email: "test@test.com") do |u|
@@ -16,3 +18,26 @@ user = User.find_or_create_by!(email: "test@test.com") do |u|
 end
 
 puts "Seed OK - user: test@test.com / password123"
+
+# création de 10 mémos
+puts 'Creating 10 memos'
+10.times do
+  memo = Memo.new(
+    name:    Faker::Educator.subject,
+    user_id: User.first.id
+  )
+  memo.save!
+end
+puts "10 memos created 💡 for user : #{User.first.email}"
+
+# création de 50 cards / 10 par mémo
+puts 'Creating 50 cards / 5 per memo'
+50.times do
+  card = Card.new(
+    ask:    Faker::Educator.subject,
+    question
+    user_id: User.first.id
+  )
+  card.save!
+end
+puts "10 memos created 💡 for user : #{User.first.email}"

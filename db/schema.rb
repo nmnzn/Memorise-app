a5cc1a67a5c6d460ebd6905840cd6d1ffb059ece
Mathis <kmzmathis@gmail.com>
2026-03-11 12:40:36 +0100

    ajout colonne prompt à DB memo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_10_095118) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_11_113925) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -36,6 +36,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_10_095118) do
   create_table "memos", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "name"
+    t.text "prompt"
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_memos_on_user_id"

57770cda1e6aebf279dcdbcc97627811396df382
Mathis <kmzmathis@gmail.com>
2026-03-11 17:23:08 +0100

    volume et profondeur de mémorisation OK - sur la new de mémo et injection dans le prompt LLM

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -10,16 +10,22 @@ class MemosController < ApplicationController
 
   def new
     @memo = Memo.new
+    @volume_collection = Memo.volume_collection
+    @profondeur_collection = Memo.profondeur_collection
   end
 
   def create
+    @volume_collection = Memo.volume_collection
+    @profondeur_collection = Memo.profondeur_collection
     @memo = Memo.new(memo_params)
     @memo.user = current_user
-
+    @volume = volume_for_system_prompt(params[:memo][:volume])
+    @profondeur = profondeur_for_system_prompt(params[:memo][:profondeur])
+    @prompt_total = "#{system_prompt(@volume, @profondeur)}#{@memo.prompt}"
     if @memo.save
       # call LLM avec @memo.prompt pour recevoir les questions/réponses
-      call_llm_for_questions_answers(@memo.prompt)
-      # méthode pour créer des cards avec le retour du LLM
+      call_llm_for_questions_answers(@prompt_total)
+      # méthode pour créer des cards avec le retour du LLM - en attente
       redirect_to memo_path(@memo), notice: "Le mémo a bien été créé."
     else
       render :new, status: :unprocessable_entity
@@ -45,31 +51,7 @@ class MemosController < ApplicationController
     end
    end
 
-  SYSTEM_PROMPT = "Tu es Memorise, une application faite pour aider l’utilisateur à se souvenir des choses, simplement et naturellement. Ton ton : clair, concis, utile. Pas de blabla. Pas de texte inutile.
 
-    Ton objectif : proposer des cards (question et réponse associée) pertinentes pour l’utilisateur, en te basant uniquement sur ce qui est explicitement présent dans la conversation : le sujet demandé (le sujet), le niveau de mémorisation souhaité (le nombre de questions = le volume de connaissance), le type de connaissances voulu si précisé (la profondeur des questions).
-
-    Méthode :
-    1) Comprends l’intention : déduis les éléments les plus utiles à mémoriser à partir du sujet demandé, du niveau de profondeur, et du nombre de questions souhaité.
-    3) Si l’utilisateur précise un nombre de questions, respecte-le, tant qu’il est compris entre 1 et 10.
-    4) Si l’utilisateur ne précise pas de nombre de questions, adapte la quantité selon le niveau demandé :
-        - léger = 3 cards
-        - moyen = 6 cards
-        - profond = 10 cards
-    5) Si le type de connaissances :
-        - grandes lignes = privilégie l’essentiel
-        - anecdotes = privilégie les détails marquants et mémorables pour maitriser le sujet dans certains détails
-    6) Si la demande est floue : ne pose pas de questions, propose quand même des cards cohérentes avec le sujet.
-    7) Ne crée pas de code ou autre élément, propose juste les questions/réponses.
-    8) Ne donne aucune explication, aucun commentaire, aucune introduction. Uniquement les cards (pour rappel une card est composée d'une question et d'une réponse).
-
-    Format de réponse (obligatoire, indiscutable, et systématique, avec un texte clair uniquement) : un array composé de hashs, où chaque hash comprend une card (question et réponse).
-    exemple :
-    [
-      { 'question': 'Question 1', 'answer': 'Réponse 1' },
-      { 'question': 'Question 2', 'answer': 'Réponse 2' },
-      { 'question': 'Question 3', 'answer': 'Réponse 3' }
-    ]"
 
   private
 
@@ -77,9 +59,54 @@ class MemosController < ApplicationController
     params.require(:memo).permit(:name, :prompt)
   end
 
+  def system_prompt(volume, profondeur)
+    return "Tu es Memorise, une application faite pour aider l’utilisateur à se souvenir des choses, simplement et naturellement. Ton ton : clair, concis, utile. Pas de blabla. Pas de texte inutile.
+      end
+
+      Ton objectif : proposer des cards (question et réponse associée) pertinentes pour l’utilisateur, en te basant uniquement sur ce qui est explicitement présent dans la conversation : le sujet demandé (le sujet), le niveau de mémorisation souhaité (le nombre de questions = le volume de connaissance), le type de connaissances voulu si précisé (la profondeur des questions).
+
+      Méthode :
+      1) Comprends l’intention : déduis les éléments les plus utiles à mémoriser à partir du sujet demandé, du niveau de profondeur, et du nombre de questions souhaité.
+      3) Si l’utilisateur précise un nombre de questions, respecte-le, tant qu’il est compris entre 1 et 10.
+      4) Génère #{volume} cards (question/réponse)
+      5) Type de questions : #{profondeur}
+      6) Si la demande est floue : ne pose pas de questions, propose quand même des cards cohérentes avec le sujet.
+      7) Ne crée pas de code ou autre élément, propose juste les questions/réponses.
+      8) Ne donne aucune explication, aucun commentaire, aucune introduction. Uniquement les cards (pour rappel une card est composée d'une question et d'une réponse).
+
+      Format de réponse (obligatoire, indiscutable, et systématique, avec un texte clair uniquement) : un array composé de hashs, où chaque hash comprend une card (question et réponse).
+      exemple :
+      [
+        { 'question': 'Question 1', 'answer': 'Réponse 1' },
+        { 'question': 'Question 2', 'answer': 'Réponse 2' },
+        { 'question': 'Question 3', 'answer': 'Réponse 3' }
+      ]"
+  end
+
   def call_llm_for_questions_answers(prompt)
-    response = RubyLLM.chat.ask("#{SYSTEM_PROMPT}#{prompt}").content
+    response = RubyLLM.chat.ask("#{@system_prompt}#{prompt}").content
     raise
   end
 
+  def volume_for_system_prompt(volume)
+    case volume
+    when @volume_collection[0]
+      "5"
+    when @volume_collection[1]
+      "10"
+    else
+      "1"
+    end
+  end
+
+  def profondeur_for_system_prompt(profondeur)
+    case profondeur
+    when @profondeur_collection[0]
+      "les grandes lignes/les éléments clés à savoir/macro/grands concepts"
+    when @profondeur_collection[1]
+      "des éléments spécifiques, précis, des anecdoques"
+    else
+      "l'essentiel"
+    end
+  end
 end

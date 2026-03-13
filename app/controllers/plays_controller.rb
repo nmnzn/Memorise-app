b29172b6bf4805ac381ea2e627fa5e142bb84613
Mathis <kmzmathis@gmail.com>
2026-03-13 15:44:17 +0100

    fix

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -2,6 +2,7 @@ class PlaysController < ApplicationController
   def show
     @cards = current_user.cards.joins(:answers).where(answers:
      { user: current_user, value: false }).shuffle
+    redirect_to root_path, notice: "vous n'avez pas de card à jouer" if @cards.empty?
     @card = @cards.first
   end
 
--- app/models/memo.rb
+++ app/models/memo.rb
@@ -1,5 +1,6 @@
 class Memo < ApplicationRecord
   belongs_to :user
   has_many :cards, dependent: :destroy
+  has_one :chat, dependent: :destroy
   validates :name, presence: true
 end

7572a1edcafd1ac15436912e2146ee6e3ddab68f
MM <maximilien.moreau@outlook.com>
2026-03-17 14:18:26 +0100

    private-public-memo-feature

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -1,9 +1,25 @@
 class Memo < ApplicationRecord
   belongs_to :user
+
   has_many :cards, dependent: :destroy, inverse_of: :memo
   has_one :chat, dependent: :destroy
+
+  has_many :memo_shares, dependent: :destroy
+  has_many :shared_users, through: :memo_shares, source: :user
 
   accepts_nested_attributes_for :cards, reject_if: :all_blank
 
   validates :name, presence: true
+
+  def accessible_by?(user)
+    self.user == user || shared_users.exists?(id: user.id) || is_public?
+  end
+
+  def owned_by?(user)
+    self.user == user
+  end
+
+  def visibility_label
+    is_public? ? "Public" : "Privé"
+  end
 end

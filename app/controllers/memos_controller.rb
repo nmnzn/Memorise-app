be6b198fd8681a5368d69ea05342e5a4d3569b03
Mathis <kmzmathis@gmail.com>
2026-03-18 23:19:14 +0100

    Revert "memo-research"
    This reverts commit fc6149f7e54ebf08e0c7e49dab85db6ab457f1e4.

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -9,10 +9,6 @@ class MemosController < ApplicationController
 
   def publics
     @memos = Memo.where(is_public: true)
-
-    if params[:query].present?
-      @memos = @memos.where("name ILIKE ?", "%#{params[:query]}%")
-    end
   end
 
   def show

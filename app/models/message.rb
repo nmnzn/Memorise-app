a907d56e937965ccf34a256e9f213610378751bb
Mathis <kmzmathis@gmail.com>
2026-03-19 16:54:16 +0100

    remise navbar chat pour Neil

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,13 +3,13 @@ class Message < ApplicationRecord
 
   acts_as_message
 
-  attr_reader :volume, :profondeur
+  # attr_reader :volume, :profondeur
 
-  def self.volume_collection
-    ["Synthétique", "Large"]
-  end
+  # def self.volume_collection
+  #   ["Synthétique", "Large"]
+  # end
 
-  def self.profondeur_collection
-    ["Grandes lignes", "Appronfondie"]
-  end
+  # def self.profondeur_collection
+  #   ["Grandes lignes", "Appronfondie"]
+  # end
 end
